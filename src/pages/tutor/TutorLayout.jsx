db1bbb736fb9f2106ecd5eb4d38083c2f83bea4a
Aadil x eDen <edenxgt@gmail.com>
2024-12-17 21:16:28 +0530

    Fix => Student is able to buy a course that the tutor has removed

--- src/pages/tutor/TutorLayout.jsx
+++ src/pages/tutor/TutorLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "../../components/MainComponents/TutorFooter";
 import { CallPopup } from "@/utils/VideoChat/CallPopup";
 import VideoCall from "@/utils/VideoChat/PopupVideoCalll";
-import Chatbot from "@/utils/EthenAI";
+// import Chatbot from "@/utils/EthenAI";
 
 const TutorLayout = () => {
   const role = "tutor";
@@ -12,7 +12,7 @@ const TutorLayout = () => {
       <Header role={role} />
       <Outlet />
       <Footer role={role} />
-      <Chatbot role={role} />
+      {/* <Chatbot role={role} /> */}
       <CallPopup />
       <VideoCall />
     </>
